3cd7d3c028c61890f9e5dcc813b2ec2f2798ad6b
Wasutin <Wasutin@ex.ua>
2016-07-12 12:22:55 +0300

    HW01-START plus

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,12 +2,15 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.UserMealWithExceed;
+import ru.javawebinar.topjava.util.UserMealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -18,10 +21,13 @@ public class MealServlet extends HttpServlet {
     private static final Logger LOG=getLogger(MealServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        LOG.debug("redirect to mealList");
+        LOG.info("getAll");
 
-//        request.getRequestDispatcher("/mealList.jsp").forward(request, response);
-        response.sendRedirect("mealList.jsp");
+//        request.getRequestDispatcher("/MEAL_LIST.jsp").forward(request, response);
+//        response.sendRedirect("MEAL_LIST.jsp");
+        List<UserMealWithExceed> userMealWithExceedList= UserMealsUtil.getWithExceeded(UserMealsUtil.MEAL_LIST, 2000);
+        request.setAttribute("mealList", userMealWithExceedList);
+        request.getRequestDispatcher("/mealList.jsp").forward(request,response);
 
     }
 }
--- src/main/webapp/mealList.jsp
+++ src/main/webapp/mealList.jsp
@@ -1,4 +1,4 @@
-<%--
+<%@ page import="ru.javawebinar.topjava.util.TimeUtil" %><%--
   Created by IntelliJ IDEA.
   User: комп
   Date: 06.07.2016
@@ -6,12 +6,41 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Meal list</title>
+    <style>
+        .normal {color: green;}
+        .exceeded {color: red;}
+    </style>
 </head>
 <body>
+<section>
+
+</section>
 <h2><a href="index.html">Home</a></h2>
 <h2>Meal list</h2>
+<hr>
+<table border="1" cellpadding="8" cellspasing="0">
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    </thead>
+    <c:forEach items="${mealList}" var="meal">
+        <jsp:useBean id="meal" scope="page" type="ru.javawebinar.topjava.model.UserMealWithExceed"/>
+        <tr class="${meal.exceed ? 'exceeded' : 'normal'}">
+            <td>
+                <%=TimeUtil.toString(meal.getDateTime())%>
+            </td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
+
+    </c:forEach>
+</table>
 </body>
 </html>
