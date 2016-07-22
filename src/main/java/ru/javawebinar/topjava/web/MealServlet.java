a9abf81857938ee645a5627bad11c37a12fb79e4
Wasutin <Wasutin@ex.ua>
2016-07-22 10:10:27 +0300

    2_4_add_spring_context.patch

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.repository.mock.InMemoryUserMealRepositoryImpl;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 import ru.javawebinar.topjava.repository.InMemoryUserMealRepository;
 import ru.javawebinar.topjava.repository.UserMealRepository;
